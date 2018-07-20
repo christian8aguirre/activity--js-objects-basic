a360f7d2e8922279d6f13ad92596082bbab73ee5
tphdev <tphdevdrop@gmail.com>
2018-07-20 09:06:25 -0500

    update

--- ex-02-partyBouncer.js
+++ ex-02-partyBouncer.js
@@ -1,10 +1,38 @@
 //-----------------------------------------
-// EXERCISE 1 : partyBouncer()
+//  partyBouncer()
 //   You are having a party where alcohol will be served
-//   and you don't want to let in underage guests or rowdy felons
+//   and you don't want to let in underage guests (18)
+//   or crazy felons
 //
 //   Take an array of objects, test to see which guests are older than 18
 //   and convert it into an array of strings with the name only.
+
+//
+//  EXAMPLE
+//   var someList = [
+//      {name: 'Bob', age: 25, isFelon: false },
+//      {name: 'Tim', age: 15, isFelon: false },
+//      {name: 'Jan', age: 25, isFelon: true },
+//      {name: 'Kat', age: 33, isFelon: false }
+//   ]
+//
+//   partyBouncer(someList)
+//     => ["Bob", "Kat"]
+//
+//------------------------------------------
+
+
+// YOUR CODE HERE
+
+
+
+
+
+
+
+
+//------------------------------------------
+// TESTS
 //------------------------------------------
 
 var listOfPeople = [
@@ -23,14 +51,32 @@ var listOfPeople = [
   { name: 'Ahmad Funchess', age: 44, isFelon: true}
 ]
 
-
 var guestList = partyBouncer(listOfPeople)
 
-console.assert(guestList.length === 10)
-console.assert(guestList[0] === "Sherry Tomkins")
-console.assert(guestList[1] === "Buffy Chang")
-console.assert(guestList[5] === "Shannon 'Bannon")
 
+console.log('\n----\npartyBouncer--Test1 \n-----')
+
+console.assert( Array.isArray(guestList) === true )
+console.assert( typeof guestList[0] === 'string' )
+console.assert( typeof guestList[1] === 'string' )
+
+
+
+console.log('\n----\npartyBouncer--Test2 \n-----')
+// "Sherry Tomkins", "Bobby Francois", "Tammy Barkley",
+//  "Rufus Johnson", "Shannon O'Bannon" should be in returned
+//   array of strings.
+
+console.assert(guestList.indexOf("Sherry Tomkins") >= 0)
+console.assert(guestList.indexOf("Bobby Francois") >= 0)
+console.assert(guestList.indexOf("Tammy Barkley") >= 0)
+console.assert(guestList.indexOf("Rufus Johnson") >= 0)
+console.assert(guestList.indexOf("Shannon O'Bannon") >= 0)
+
+
+console.log('\n----\npartyBouncer--Test2\n-----')
+// 'Jimmy Dreyfus', 'Romy Podolski', 'Salomon Marzoco' should not be
+//  in returned array of strings
 console.assert(guestList.indexOf('Jimmy Dreyfus') === -1)
 console.assert(guestList.indexOf('Romy Podolski') === -1)
 console.assert(guestList.indexOf('Salomon Marzococo') === -1)
